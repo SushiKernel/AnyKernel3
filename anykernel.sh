f707d742f64a96232189d41b88ba09c5c1c4fb3b
humandroid-2002 <humandroid-2002@users.noreply.github.com>
2026-09-16 19:31:01 -0300

    bangkk: add DLKM flashing support for AOSP recovery
    commit based on https://github.com/humandroid-2002/AnyKernel3/commit/e917055ff2891f0b8ca7f565ce8ddcd82deeed40
    
    Co-authored-by: humandroid-2002 <humandroid-2002@users.noreply.github.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,35 +2,52 @@
 # osm0sis @ xda-developers
 
 properties() { '
-kernel.string=MoeKernel by @whyakari
+kernel.string=SushiKernel by @whyakari
 do.devicecheck=1
 do.modules=1
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=bangkk
-supported.versions=15.0-16.0
-supported.patchlevels=
+supported.versions=15.0-17.0
 '; }
 
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+}
+
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 . tools/ak3-core.sh;
+. $AKHOME/resize_vendor.sh
 
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+sleep 2
 
 dump_boot;
 write_boot;
 
-block=vendor_boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+vendor_boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+}
+
+BLOCK=vendor_boot;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 reset_ak;
-split_boot;
-flash_boot;
+dump_boot;
+
+if [ -d "vendor_ramdisk/lib/modules" ]; then
+  rm -rf "$RAMDISK/lib/modules";
+  mkdir -p "$RAMDISK/lib/modules";
+
+  cp -af vendor_ramdisk/lib/modules/. "$RAMDISK/lib/modules/";
+fi;
+
+write_boot;
